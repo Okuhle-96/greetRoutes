b6a1e95d37ec0219f16559b31fbe8ad5510c38f9
Okuhle_Mhambi <bukhosimhambi@gmail.com>
2021-08-18 11:15:42 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const greetLangRadio = require("./greet-factory");
+const helperfunction = require('./helper');
+const exphbs  = require('express-handlebars');
+const handlebarSetup = exphbs({
+    partialsDir: "./views/partials",
+    viewPath:  './views',
+    layoutsDir : './views/layouts'
+});
+var bodyParser = require('body-parser');
+
+app.use(express.static('public'));
+
+let helper = helperfunction();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.engine('handlebars', handlebarSetup);
+app.set('view engine', 'handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main', layoutsDir:__dirname + '/views/layouts'}));
+app.set('view engine', 'handlebars');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(express.urlencoded({extended: false}));
+
+
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        greeting : helper.getMsg(),
+        counter : helper.getCounter(),
+        errormessages : helper.allErrors()
+    });
+});
+
+app.post('/greeted', (req, res) => {
+    helper.greeting(req.body.Names, req.body.languageRadio);
+
+    res.redirect('/')
+});
+
+app.post('/greeting', (req, res) => {
+
+    res.send("thanks for greeting this name.")
+});
+
+app.get("/results", (req, res) => {
+    res.send("please enter a correct name!")
+})
+
+const PORT = process.env.PORT || 30013; //Make my port number configurable
+
+app.listen(PORT, function () {
+    console.log("App Started at", PORT)
+});
